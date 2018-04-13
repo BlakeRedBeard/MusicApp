9d23a0d742458903cdc380c3a69a291ddecf9203
DESKTOP-972F69U\Daniele <blake99@live.it>
2018-04-13 22:28:12 +0200

    prima commit della fase di develop.

--- src/server.java
+++ src/server.java
@@ -1,4 +1,24 @@
+import java.net.*;
+
 
 public class server {
 
+	private Socket connection;
+	private ServerSocket serverSocket;
+	private int port;
+	
+	public server() {
+		this.connection = null;
+		this.port = 2440;
+		
+		startServer();
+	}
+	
+	
+	private void startServer() {
+		
+	}
+	
+	
+	
 }
