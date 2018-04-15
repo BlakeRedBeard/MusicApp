420a2d67da8da28f6a2a7d58bb827dada56bdf95
DESKTOP-972F69U\Daniele <blake99@live.it>
2018-04-15 11:50:29 +0200

    inizio implementazione struttura

--- src/Node.java
+++ src/Node.java
@@ -1,55 +1,81 @@
+import java.util.ArrayList;
 
 public class Node {
-	 private Integer informazione;
-	 private Node sx, dx;
-	 private int posizione;
+	 
+	 private ArrayList<Node> sons;   //array contenente i nodi figli, essi fanno parte tutti dello stesso livello
+	 private Node father;			 //nodo padre, nel caso del primo nodo esso avrà valore nullo
+	 private String path, name;		 //le informazioni che si vogliono salvare nel nodo
+	 
+	 
+	 public Node() {
+		 this.sons = null;
+		 this.setFather(null);
+		 this.setPath(null);
+		 this.setName(null);
+	 }
+	 
+	 public Node(Node father) {
+		 this.sons = null;
+		 this.setFather(father);
+		 this.setPath(null);
+		 this.setName(null);
+	 }
+	 
+	 public Node(String path, String name) {
+		 this.sons = null;
+		 this.setFather(null);
+		 this.setPath(path);
+		 this.setName(name);
+	 }
+	 
+	 public Node(Node father, String path, String name) {
+		 this.sons = null;
+		 this.setFather(father);
+		 this.setPath(path);
+		 this.setName(name);
+	 }
 
-	     
-	    public Node(int posizione){
-	    	this.posizione = posizione;
-	        this.setInf(0);
-	        setSx(null);
-	        setDx(null);
-	    }
-	     
-	    public Node(int posizione, int number) 
-	    {
-	    	this.posizione = posizione;
-	        this.setInf(number);
-	    }
-	    
-	    public int getPos(){
-	    	return this.posizione;
-	    }
 
-		public int getInf(){
-			return informazione;
-		}
-		
-		public void setInf(int informazione){
-			this.informazione = informazione;
-		}
-		
-		public void setInf(Integer informazione){
-			this.informazione = informazione;
-		}
+	public Node getFather() {
+		return father;
+	}
 
-		public Node getSx(){
-			return sx;
-		}
 
-		public void setSx(Node sx){
-			this.sx = sx;
-		}
+	public void setFather(Node father) {
+		this.father = father;
+	}
 
-		public Node getDx(){
-			return dx;
-		}
 
-		public void setDx(Node dx){
-			this.dx = dx;
-		}
-	    
-	    
-	    
+	public String getPath() {
+		return path;
+	}
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public void addSon(Node son) {
+		this.sons.add(son);
+		son.setFather(this);
+	}
+	
+	public void addSon(String path, String name) {
+		this.sons.add(new Node(this, path, name));
+	}
+	
+	
+	 
+	 
 }
